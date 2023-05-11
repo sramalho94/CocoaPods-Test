a41bf66013e486ce7adc8ff98c57648cb8e15bd9
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-11 12:33:05 -0400

    building out paywall screen

--- screens/Paywall.tsx
+++ screens/Paywall.tsx
@@ -1,11 +1,37 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import React from 'react';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import {useNavigation} from '@react-navigation/native';
+
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Paywall'
+>;
 
 const Paywall = () => {
+  const navigation = useNavigation<NavigationProp>();
+
   return (
-    <View>
-      <Text>Paywall</Text>
-    </View>
+    <ScrollView className="bg-[#1A2F44] flex-1">
+      <View className="m-10 space-y-2">
+        <Text className="text-2xl text-center uppercase text-white font-bold">
+          upgrade
+        </Text>
+        <Text className="text-center text-white">
+          Upgrade to Pro to Access all the Features
+        </Text>
+      </View>
+      <TouchableOpacity>
+        <Icon
+          name="md-close-sharp"
+          size={32}
+          color="#E5962D"
+          onPress={navigation.goBack}
+        />
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
