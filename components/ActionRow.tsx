44da14cd6b499c7debc9161201098c9ddb1fc1c4
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-11 12:23:06 -0400

    built out demoscreen and more styling for homescreen

--- components/ActionRow.tsx
+++ components/ActionRow.tsx
@@ -1,11 +1,43 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import {useNavigation} from '@react-navigation/native';
 
-const ActionRow = () => {
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type Props = {
+  title: string;
+  screen: any;
+  color: string;
+  requiresPro?: boolean;
+  icon?: any;
+  vertical?: boolean;
+};
+
+const ActionRow = ({
+  title,
+  screen,
+  color,
+  requiresPro,
+  icon,
+  vertical,
+}: Props) => {
+  const navigation = useNavigation<NavigationProp>();
   return (
-    <View>
-      <Text>ActionRow</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => navigation.navigate(screen)}
+      className={`flex flex-1 justify-center items-center py-6 rounded-lg space-x-2 m-2  ${
+        vertical ? 'flex-col' : 'flex-row'
+      }`}
+      style={{backgroundColor: color}}>
+      <Icon name={icon} size={30} color="white" />
+      <Text className="text-white font-bold text-lg">{title}</Text>
+    </TouchableOpacity>
   );
 };
 
--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,11 +1,39 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import {RootStackParamList} from '../App';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Demo'
+>;
 const DemoScreen = () => {
+  const navigation = useNavigation<NavigationProp>();
   return (
-    <View>
-      <Text>DemoScreen</Text>
-    </View>
+    <SafeAreaView className="bg-papa-orange flex-1">
+      <TouchableOpacity
+        className="flex-row items-center p-5"
+        onPress={navigation.goBack}>
+        <Icon name="arrow-back" size={50} color="white" />
+        <Text className="text-white">Go Back</Text>
+      </TouchableOpacity>
+      <View className="flex-1 items-center justify-center px-10">
+        <Text className="text-white text-2xl front-extrabold">Hooray!</Text>
+        <Text className="text-white text-2xl font-extrabold mb-20">
+          You have access to this feature
+        </Text>
+        <Icon name="build-outline" size={200} color="white" />
+        <View className="-mt-16 -pl-8">
+          <Icon name="checkmark-circle-sharp" size={60} color="#96F550" />
+        </View>
+        <Text className="text-white mt-10 flex-1 font-bold text-center">
+          This is where you create a new screen & change the rest of the app so
+          the navigation is as follows: navigation.navigate('ThePageYouWant')
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
